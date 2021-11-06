dd5e7eca5a4aef4bd61121bbdcd8f07c78b32432
dan-rhul <75696965+dan-rhul@users.noreply.github.com>
2021-11-06 10:10:34 +0000

    Framework to support stored rules (flows) that can be actioned on future messages

--- reddit-modmail/RedditModmail.py
+++ reddit-modmail/RedditModmail.py
@@ -1,6 +1,8 @@
 import asyncio
+from enum import Enum, unique, auto
 from threading import Thread
 import logging
+from typing import Optional, Tuple, Union, OrderedDict
 
 import asyncpraw
 from asyncpraw.models import ModmailConversation
@@ -11,6 +13,57 @@ from ruamel.yaml.comments import CommentedMapItemsView
 logging.basicConfig(
     level=logging.INFO
 )
+
+
+@unique
+class Rule(Enum):
+    ACTION: auto()
+    COMMENT: auto()
+
+    CONTENT: auto()
+    SUBJECT: auto()
+
+    AUTHOR_POST_KARMA: auto()
+    AUTHOR_COMMENT_KARMA: auto()
+    AUTHOR_COMBINED_KARMA: auto()
+    AUTHOR_ACCOUNT_AGE: auto()
+    AUTHOR_SATISFY_ANY_THRESHOLD: auto()
+
+    CONTENT_LONGER_THAN: auto()
+    CONTENT_SHORTER_THAN: auto()
+
+    AUTHOR_IS_CONTRIBUTOR: auto()
+    AUTHOR_IS_MODERATOR: auto()
+
+    IS_TOP_LEVEL: auto()
+    TYPE: auto()
+
+    def as_yaml_key(self) -> str:
+        if str.startswith(self.name, "AUTHOR"):
+            key = str.replace(self.name, "AUTHOR_", "author.")
+        else:
+            key = self.name
+
+        key = str.lower(key)
+        return key
+
+    def get_priority(self) -> int:
+        return self.value
+
+
+class RuleActions:
+    @staticmethod
+    def parse(conditions: Tuple[str, Union[str, OrderedDict]]) -> 'RuleActions':
+        pass
+
+    def __init__(self):
+        self.actions = []
+
+    def should_action(self) -> bool:
+        pass
+
+    def run(self):
+        pass
 
 
 class RedditModmail:
@@ -39,18 +92,41 @@ class RedditModmail:
     async def _listen_state(self, subreddit: Subreddit, state: str):
         async for message in subreddit.mod.stream.modmail_conversations(sort="recent",
                                                                         state=state):
-            self.handle_message(subreddit, message, state)
+            await self.handle_message(subreddit, message, state)
 
-    def handle_message(self, subreddit: Subreddit, message: ModmailConversation, state: str):
-        pass
+    async def handle_message(self, subreddit: Subreddit, message: ModmailConversation, state: str):
+        conversation = await subreddit.modmail(message.id)
+        lastMessage = conversation.messages[-1].body_markdown
+
+        config = self.parse_config_yaml(await self.get_config(subreddit, "reddit_modmail"))
+        rules = self.get_rules_of_type(state, config)
+        if len(rules) == 0:
+            return
+
+        rule: Optional[Rule]
+        rule = None
+        for rule_pair in rules:
+            rule = Rule.parse(rule_pair[1].items())
+
+        if rule is None:
+            return
+
+        if rule.should_action():
+            rule.run()
+
+    def get_rules_of_type(self, rule_type: str, rules: CommentedMapItemsView) -> []:
+        type_rules = []
+
+        for item in rules:
+            for key, value in item[1].items():
+                if key == "type" and (isinstance(value, str) and value == rule_type) or (isinstance(value, list) and rule_type in value):
+                    type_rules.append(item)
+                    break
+
+        return type_rules
 
     async def get_config(self, subreddit: Subreddit, wiki_page: str) -> str:
         return await subreddit.wiki.get_page(wiki_page).content_md
 
     def parse_config_yaml(self, text: str) -> CommentedMapItemsView:
         return self.yaml.load(text).items()
-        # for item in self.yaml.load(text).items():
-        #     print(item[0])
-        #     for rule, value in item[1].items():
-        #         print(rule)
-        #         print(value)
